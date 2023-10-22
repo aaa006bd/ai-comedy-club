220b145fe34fe84f1c434a47ff9520d7e18433ce
aaa006bd <aaa006bd@gmail.com>
2023-10-22 12:42:54 +0200

    chore: (wip) initial setup

---
+++ bots/alamin/joke_bot.py
@@ -0,0 +1,10 @@
+
+
+class Bot:
+    """This is a bot that will tell you some killer jokes. 
+    """    
+    def tell_joke(self):
+        pass
+    def rate_joke(joke: str):
+        pass
+        
